bebe74cfb6b98708a9d2684506fc6e9f15895d52
Baroslucas <lucas.baros41@gmail.com>
2025-06-11 12:08:03 +0200

    update(component): Update the ShowInfoDiv to CretedBy

---
+++ src/components/CreatedBy.jsx
@@ -0,0 +1,9 @@
+export function ShowCReatedBy({AuthorName}) {
+
+    return (
+        <div>
+            <h4>Created by</h4>
+            <span>{AuthorName}</span>
+        </div>
+    )
+}
--- src/components/ShowInfoDiv.jsx
+++
@@ -1,8 +0,0 @@
-export function ShowInfoDiv({infoCategorie, infoSpan}) {
-    return (
-        <div>
-            <h4>{infoCategorie}</h4>
-            <span>{infoSpan}</span>
-        </div>
-    )
-}

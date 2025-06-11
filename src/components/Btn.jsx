49c8481bbb927c5adc76255899bc073b01ec708b
Baroslucas <lucas.baros41@gmail.com>
2025-06-11 10:40:01 +0200

    feat(components) add Btn and MainSearchBar components

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,5 +1,8 @@
 export function Btn() {
     return <button>
-        
+        {Icon && (
+            <img src={iconSrc} alt={alt} className="" /> 
+        )}
+        <span>{label}</span>
     </button>
 }
---
+++ src/components/MainSearchBar.jsx
@@ -0,0 +1,3 @@
+export function MainSearchBar() {
+    return <input placeholder='Search a TV show you may like' type="text" value={value} onChange={onChange} />
+}
